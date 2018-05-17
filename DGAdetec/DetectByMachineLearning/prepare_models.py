d3aecccbf38bacb7b0f5db3b1ccb301e3eda7f7c
zhouyc <yuanchenzhouhcmy@gmail.com>
2018-05-17 17:50:11 +0800

    Update prepare_models.py
    add parameters

--- DGAdetec/DetectByMachineLearning/prepare_models.py
+++ DGAdetec/DetectByMachineLearning/prepare_models.py
@@ -5,9 +5,15 @@ import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.externals import joblib
 
-def n_grame_train(big_domain):
-	vec = CountVectorizer(min_df=1, ngram_range=(1,2), token_pattern=r"\w")
+def n_grame_train(big_domain, grame_range, model_name):
+	"""parameters:
+	big dimain: domains waiting to bed fitted
+	grame_range: a tuple
+	model_name: string end with '.pkl'
+
+	"""
+	vec = CountVectorizer(min_df=1, ngram_range=grame_range, token_pattern=r"\w")
 	grame_model = vec.fit(big_domain)
-	joblib.dump(grame_model, './models/n_grame.pkl')
+	joblib.dump(grame_model, './models/'+model_name)
 
 
